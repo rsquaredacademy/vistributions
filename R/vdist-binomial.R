5e5b0b44726cd4aa4ff44097a038be41f4a51e2c
rsquaredin <rsquaredin@gmail.com>
2018-11-16 15:10:17 +0530

    remove plotly

--- R/vdist-binomial.R
+++ R/vdist-binomial.R
@@ -6,8 +6,6 @@
 #'
 #' @param n Number of trials.
 #' @param p Aggregate probability.
-#' @param lib The library to be use for visualization. \code{ggplot2} is the
-#' default; the other option is \code{plotly}.
 #'
 #' @examples
 #' # visualize binomial distribution
@@ -19,7 +17,7 @@
 #'
 #' @export
 #'
-vdist_binom_plot <- function(n, p, lib = c("ggplot2", "plotly")) {
+vdist_binom_plot <- function(n, p) {
 
   if (!is.numeric(n)) {
     stop("n must be numeric/integer")
@@ -52,35 +50,16 @@ vdist_binom_plot <- function(n, p, lib = c("ggplot2", "plotly")) {
   data <- stats::dbinom(x, n, p)
   plot_data <- tibble::tibble(n = seq(0, n), df = data)
 
-  use_lib <- match.arg(lib)
-
-  if (use_lib == "ggplot2") {
-    plot_data %>%
-      ggplot2::ggplot() +
-      ggplot2::geom_col(ggplot2::aes(x = n, y = df), fill = "blue") +
-      ggplot2::ylab("Probability") + ggplot2::xlab("No. of success") +
-      ggplot2::ggtitle(label = paste("Binomial Distribution: n =", n, ", p =", p),
-              subtitle = paste("Mean =", bm, ", Std. Dev. =", bsd)) +
-      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
-            plot.subtitle = ggplot2::element_text(hjust = 0.5)) +
-      ggplot2::scale_x_continuous(breaks = seq(0, n))
-  } else if (use_lib == "plotly") {
-    plotly::plot_ly(plot_data, x = ~n, y = ~df) %>%
-      plotly::add_bars(color = I("blue")) %>%
-      plotly::layout(title = paste("Binomial Distribution: n =", n, ", p =", p),
-             xaxis = list(title = "No. of success"),
-             yaxis = list(title = "Probability"),
-             annotations = list(
-               list(x = xn, y = 1.05, showarrow = F,
-                    text = paste("Mean =", bm, ", Std. Dev. =", bsd),
-                    xref='paper', yref='paper')))
-
-  } else {
-    stop("Please specify a valid library.", call. = FALSE)
-  }
+  plot_data %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_col(ggplot2::aes(x = n, y = df), fill = "blue") +
+    ggplot2::ylab("Probability") + ggplot2::xlab("No. of success") +
+    ggplot2::ggtitle(label = paste("Binomial Distribution: n =", n, ", p =", p),
+                     subtitle = paste("Mean =", bm, ", Std. Dev. =", bsd)) +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
+                   plot.subtitle = ggplot2::element_text(hjust = 0.5)) +
+    ggplot2::scale_x_continuous(breaks = seq(0, n))
 
 }
-
-
 
 
