70ee893296d433d594ca31a5a332be54491da45e
rsquaredin <rsquaredin@gmail.com>
2018-11-20 15:43:36 +0530

    fix cran note and warnings

--- tests/testthat/test-chisquare.R
+++ tests/testthat/test-chisquare.R
@@ -19,16 +19,3 @@ test_that("vdist_chisquare_perc returns appropriate error messages", {
 
   expect_error(vdist_chisquare_perc(df = as.factor(3)), "df must be numeric/integer")
 })
-
-
-test_that("chisquare perc plot is as expected", {
-  skip_on_cran()
-  gplot <- vdist_chisquare_plot(df = 5)
-  vdiffr::expect_doppelganger("chi_perc", gplot)
-})
-
-test_that("chisquare perc normal plot is as expected", {
-  skip_on_cran()
-  gplot <- vdist_chisquare_plot(df = 5, normal = TRUE)
-  vdiffr::expect_doppelganger("chi_perc_normal", gplot)
-})
