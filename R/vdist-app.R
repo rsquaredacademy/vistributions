3e5d6af88c1e85d3e8d876614a5d02a31f2d1581
rsquaredin <rsquaredin@gmail.com>
2019-01-23 21:46:28 +0530

    import app from xplorerr

---
+++ R/vdist-app.R
@@ -0,0 +1,21 @@
+#' @importFrom shiny runApp
+#' @title Launch shiny app
+#' @description Launches shiny app for interactive model building.
+#' @examples
+#' \dontrun{
+#' vdist_launch_app ()
+#' }
+#' @export
+#'
+vdist_launch_app  <- function() {
+
+	message("`vdist_launch_app ()` has been soft-deprecated and will be removed in the next release. In future, to launch the app, run the below code:\n 
+	- install.packages('xplorerr')\n - xplorerr::app_vistributions()\n")
+
+	check_suggests('shinyBS')
+	check_suggests('shinycssloaders')
+	check_suggests('shinythemes')
+
+	xplorerr::app_vistributions()
+}
+ 
