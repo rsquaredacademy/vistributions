cbf2131a488660fe7cdd29d1b7a9d04bc2b779a6
rsquaredin <rsquaredin@gmail.com>
2019-01-24 18:11:59 +0530

    option to update package

--- R/zzz.R
+++ R/zzz.R
@@ -21,10 +21,16 @@
 
   tip <- sample(tips, 1)
 
-  if (behind_cran) {
-    packageStartupMessage("A new version of vistributions (0.2.0) is available with bug fixes and new features.")
-  } else {
-    packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+  if (interactive()) {
+    if (behind_cran) {
+      msg <- message("A new version of vistributions is available with bug fixes and new features.")
+      message(msg, "\nWould you like to install it?")
+      if (utils::menu(c("Yes", "No")) == 1) {
+        utils::update.packages("vistributions")
+      } 
+    } else {
+      packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+    }   
   }   
 
 }
