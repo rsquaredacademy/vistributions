d0ac3c32325b4f39eba70ebe117759430622027e
rsquaredin <rsquaredin@gmail.com>
2020-12-02 22:05:21 +0530

    add functions to validate inputs

--- R/vdist-utils.R
+++ R/vdist-utils.R
@@ -1,12 +1,12 @@
 #' @importFrom utils packageVersion menu install.packages
 check_suggests <- function(pkg) {
-  
+
   pkg_flag <- tryCatch(utils::packageVersion(pkg), error = function(e) NA)
-  
+
   if (is.na(pkg_flag)) {
-    
+
     msg <- message(paste0('\n', pkg, ' must be installed for this functionality.'))
-    
+
     if (interactive()) {
       message(msg, "\nWould you like to install it?")
       if (utils::menu(c("Yes", "No")) == 1) {
@@ -16,7 +16,31 @@ check_suggests <- function(pkg) {
       }
     } else {
       stop(msg, call. = FALSE)
-    } 
+    }
   }
 
+}
+
+check_numeric <- function(val, var = "n") {
+  if (!is.numeric(val)) {
+    stop(paste(var, "must be numeric."), call. = FALSE)
+  }
+}
+
+check_logical <- function(val, var = "normal") {
+  if (!is.logical(val)) {
+    stop(paste(var, "must be a logical value."), call. = FALSE)
+  }
+}
+
+check_positive <- function(val, var = "sd") {
+  if (val < 0) {
+    stop(paste(var, "must be a positive value."), call. = FALSE)
+  }
+}
+
+check_range <- function(val, min = 0, max = 1, var = "p") {
+  if ((val < min) | (val > max)) {
+    stop(paste(var, "must be between", min, "and", max, "only."), call. = FALSE)
+  }
 }
